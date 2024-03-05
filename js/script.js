f1ae0119f94543ae4e3aebbda72d699a6f9cb724
Carlos Roberto <crgfilho15@gmail.com>
2024-03-05 19:23:23 -0300

    Atualizando

--- js/script.js
+++ js/script.js
@@ -18,12 +18,18 @@ $(document).ready(function() {
             let minutos = $("#campo-tempo-uso-minutos-aparelho").val() == "" ? "00" : $("#campo-tempo-uso-minutos-aparelho").val().padStart(2, "0");
             let horas_aux = $("#campo-tempo-uso-horas-aparelho").val() == "" ? "min" : "hora(s)";
 
+            if (horas == "01") {
+                horas_aux = "hora";
+            }
+
             let aparelhos_cadastrados = $("#tabela-aparelhos-cadastrados").html();
 
-            aparelhos_cadastrados += `<tr>`;
+            aparelhos_cadastrados += `<tr id="${$("#campo-nome-aparelho").val()}">`; // TO DO: VERIFICAR SE O ID JÁ EXISTE ANTES DE CRIAR (NÃO PODE REPETIR). POSSO CRIAR UM ARRAY DE ID'S E VERIFICAR
             aparelhos_cadastrados += `<td>${$("#campo-nome-aparelho").val()}</td>`;
             aparelhos_cadastrados += `<td>${$("#campo-potencia-aparelho").val()}</td>`;
             aparelhos_cadastrados += `<td>${horas}:${minutos} ${horas_aux}/dia</td>`
+            aparelhos_cadastrados += `<td>R$ X/dia</td>`
+            aparelhos_cadastrados += `<td>R$ X/mês</td>`
             aparelhos_cadastrados += `<td class="botao-tabela"><i class="fa-solid fa-pen-to-square botao-editar"></i></td>`;
             aparelhos_cadastrados += `<td class="botao-tabela"><i class="fa-solid fa-trash botao-excluir"></i></td>`;
             aparelhos_cadastrados += `</tr>`;
@@ -33,11 +39,10 @@ $(document).ready(function() {
             $("#campo-potencia-aparelho").val("");
             $("#campo-tempo-uso-horas-aparelho").val("");
             $("#campo-tempo-uso-minutos-aparelho").val("");
-
         } catch (err) {
             alert(err);
         }
     });
 
-    
+
 });
